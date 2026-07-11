696ddb720a4d7264b547828cf06a3b5585d35187
alexroessner <aroessner1@gmail.com>
2026-07-10 21:40:39 -0400

    fix(catalogue): optics up 25 left 15; acoustic pod up another 5

--- src/main.js
+++ src/main.js
@@ -140,8 +140,8 @@ const DEVICES = [
       ['Spec sheet', 'in development'],
     ],
     callouts: [
-      ['lens', 'Optics', 'VillageGuard 2 MP platform', -30, -45],
-      ['pod', 'Acoustic pod', 'The listening half of the survey', -45, -5, 'above'],
+      ['lens', 'Optics', 'VillageGuard 2 MP platform', -45, -70],
+      ['pod', 'Acoustic pod', 'The listening half of the survey', -45, -10, 'above'],
       ['ai', 'Bespoke models', 'The key species of your landscape', 165, -5],
       ['wifi', 'Wi-Fi offload', 'No airtime required', 49, -28, null, 1],
     ],
