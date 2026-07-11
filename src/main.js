347cd7c7d821d0e7be02e1837f7bc7a69e3f9994
alexroessner <aroessner1@gmail.com>
2026-07-10 21:21:19 -0400

    fix(catalogue): bespoke anchor dot sits on the camo flank itself

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@
 
 import * as THREE from 'three';
 import { createWorld } from './world.js?v=3';
-import { BUILDERS } from './devices.js?v=11';
+import { BUILDERS } from './devices.js?v=12';
 
 const $ = (s) => document.querySelector(s);
 
